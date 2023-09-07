31af01b68e2a8ce7c254674e7f7d4e0c4f298409
CodeMasterAkash <akashgaikwad9225@gmail.com>
2023-09-08 00:42:51 +0530

    First Commit.

---
+++ JPQL/src/main/java/com/jspiders/JPQL/DAO/EmployeeDAO.java
@@ -0,0 +1,55 @@
+package com.jspiders.JPQL.DAO;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.jspiders.JPQL.DTO.EmployeeDTO;
+
+public class EmployeeDAO {
+
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static EntityTransaction entityTransaction;
+
+	private static void openConnection() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("alphahibernate");
+		entityManager = entityManagerFactory.createEntityManager();
+		entityTransaction = entityManager.getTransaction();
+	}
+
+	private static void closeConnection() {
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+		}
+		if (entityManager != null) {
+			entityManager.close();
+		}
+		if (entityTransaction != null) {
+			if (entityTransaction.isActive()) {
+				entityTransaction.rollback();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		openConnection();
+		entityTransaction.begin();
+
+		Query query = entityManager.createQuery("Select emp from EmployeeDTO emp");
+
+		@SuppressWarnings("unchecked")
+		List<EmployeeDTO> employees = query.getResultList();
+
+		for (EmployeeDTO employee : employees) {
+			System.out.println(employee);
+		}
+		entityTransaction.commit();
+		closeConnection();
+	}
+}
